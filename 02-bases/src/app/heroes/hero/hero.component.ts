d36977c398893f6d0d6c4273362160cee08a4002
rick <rick178@hotmail.com>
2024-11-22 23:18:18 -0600

    Continue Learning 48. Directiva *ngIf

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+
   public name: string = 'ironman';
   public age:  number = 45;
+
+  get capitalizedName(): string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeName(): void{
+    this.name = 'Spiderman';
+  }
+  changeAge(): void{
+    this.age = 20;
+  }
+  resetForm(): void{
+    this.name = 'ironman';
+    this.age = 45;
+  }
 }
