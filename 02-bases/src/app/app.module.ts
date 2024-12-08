36ad2798e8d9c17d067af5bcf048e450e1340f8b
rick <rick178@hotmail.com>
2024-12-08 04:16:10 -0600

    Continue learning 52. Tarea sobre módulos

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -3,20 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { CounterComponent } from './counter/components/counter/counter.component';
+import { CounterModule } from './counter/components/counter/counter.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 02-bases/src/app/heroes/components/heroes.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { ListComponent } from "./list/list.component";
+import { HeroComponent } from "./hero/hero.component";
+
+
+
+@NgModule({
+  declarations: [
+    HeroComponent,
+    ListComponent
+  ],
+  exports: [
+    HeroComponent,
+    ListComponent
+  ]
+})
+
+export class heroesModule{
+
+}
